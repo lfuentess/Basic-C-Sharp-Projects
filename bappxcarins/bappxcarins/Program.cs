a7b634d69ea21acf2270260163bdfc9b5e729f6b
lfuentess <lfuentessa@gmail.com>
2026-08-11 10:34:12 -0400

    basic approval program for car insurance
    Boolean Logic
    - Test evaluate condition for approval or not car insurance
    - Requiere data for evaluation
     -- age
     -- quantity DUI
     -- quantity speed tickets

---
+++ bappxcarins/bappxcarins/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace bappxcarins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Approval program for Car Insurance \n");//show identified application program
+            /* show question and recive answer*/
+            Console.WriteLine("What is your age?"); 
+            int iAge = Convert.ToInt32(Console.ReadLine()); //set integer variable and convert string data to integer data
+            Console.WriteLine("Have you ever had a DUI?");
+            bool bDUI = Convert.ToBoolean(Console.ReadLine());//set boolean variable and convert string data to boolean data
+            Console.WriteLine("How many speeding tickets do you have?");
+            int iTickets = Convert.ToInt32(Console.ReadLine());//set integer variable and convert string data to integer data
+
+            bool bQualified = false; //set variable of approval or denied car insurance
+            bQualified = (bDUI == false && iAge > 15 && iTickets < 4);// evaluate all data is agree with approval and assign value
+
+            Console.WriteLine("Qualified for insurance? \n" + bQualified.ToString());//show the result of evaluation of data 
+            Console.Read();
+        }
+    }
+}
---
+++ bappxcarins/bappxcarins/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("bappxcarins")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ReviOS 11 25.10")]
+[assembly: AssemblyProduct("bappxcarins")]
+[assembly: AssemblyCopyright("Copyright © ReviOS 11 25.10 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("448693ba-5d5b-4c05-92de-1bff2193de1f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
