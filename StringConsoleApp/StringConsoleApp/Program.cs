ba583274b5ae13ef80fbd2fff9c6a1489a1543e5
lfuentess <lfuentessa@gmail.com>
2026-08-12 13:30:24 -0400

    String Assignment
    String manipulate
    - concatenation
    - convert to text in upper
    - use StringBuilder
    - comments

---
+++ StringConsoleApp/StringConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace StringConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sVarA="Hi!, today is a beautiful day! \n ", sVarB = " \t How are you doing? \n", sVarC = " \t \t Are you ok? \n"; // delarations variables and assign text
+            string sVarD = sVarA + sVarB + sVarC; // Concatenate and assign a new variable
+            Console.WriteLine(sVarD); // Display text 
+            Console.WriteLine(sVarD.ToUpper()); //Display text in uppercase
+            Console.WriteLine("-------------");
+
+            StringBuilder sbVariable = new StringBuilder(); //Create a new StringBuilder variable
+            sbVariable.Append(sVarA); //Add first variable
+            sbVariable.Append(' '); //Add blank space 
+            sbVariable.Append(sVarB);//Add second variable
+            sbVariable.Append(' ');//Add blank space 
+            sbVariable.Append(sVarC);//Add third variable
+            sbVariable.Append(' ');//Add blank space 
+            Console.WriteLine(sbVariable.ToString());//Display the StringBuilder variable
+
+            sbVariable.Insert(0, "This go first. \n");// Insert text in the first position
+
+            Console.WriteLine(sbVariable.ToString());// Display the StringBuilder variable
+
+            sbVariable.Replace("Hi", "Hello"); // Change one word to another
+            Console.WriteLine(sbVariable.ToString());// Display the StringBuilder variable
+        }
+    }
+}
