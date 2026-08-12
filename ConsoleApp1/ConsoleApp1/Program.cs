5710cb691e460b0b2c2cc098db62b82f973828c4
lfuentess <lfuentessa@gmail.com>
2026-08-12 12:24:15 -0400

    Console App
    This console app if just to try how use while and do.. while  statements

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int iNum=20; // define int variable and assign value
+            while (iNum >0) // set loop and condition to keep inside
+            {
+                Console.WriteLine("counting: " + iNum.ToString()); // show message and variable value 
+                iNum--; // reduce value variable
+            }
+            Console.WriteLine("counting finished"); // show message 
+
+            do // set another loop 
+            {
+                Console.WriteLine("counting (again): " + iNum.ToString()); // show message and variable value 
+                iNum++; // increse value variable
+            } while (iNum <= 20); // condition to do until
+
+            Console.WriteLine("counting finished");// show message 
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ReviOS 11 25.10")]
+[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyCopyright("Copyright © ReviOS 11 25.10 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7db2cf2a-11ad-4c45-8bd3-7e8f2191e996")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
