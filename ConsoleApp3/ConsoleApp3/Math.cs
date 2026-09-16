ef13b23bd5bb076f3945041ba1d519c99b779e7d
lfuentess <lfuentessa@gmail.com>
2026-09-16 19:46:31 -0400

    Calling Methods Assignment
    Create three methods, each of which will take one integer parameter in and return an integer.

---
+++ ConsoleApp3/ConsoleApp3/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Math
+    {
+        //Math Methods that take a integer and return a integer result
+        public int Sum(int value)
+        {
+            return (value + 5); 
+        }
+        public int Sub(int value) 
+        { 
+            return (value - 10 );
+        }
+
+        public int Mul(int value) 
+        {
+            return ( value * 6 );
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calling Methods
+            
+            //create math object 
+            Math math = new Math();
+            
+            //require from user a number to perform operatios
+            Console.WriteLine("Enter a number to perform mathematical operations.");
+            int valEntry = Convert.ToInt32(Console.ReadLine());
+
+            //calls each methods send int value and return result value to integer 
+            int result0 = math.Sum(valEntry);
+            int result1 = math.Sub(valEntry);
+            int result2 = math.Mul(valEntry);
+
+            //show each operations performed with the result 
+            Console.WriteLine(valEntry + " +  5 = " +result0);
+            Console.WriteLine(valEntry + " - 10 = " + result1);
+            Console.WriteLine(valEntry + " *  6 = " + result2);
+
+            Console.ReadLine();
+        }
+    }
+}
