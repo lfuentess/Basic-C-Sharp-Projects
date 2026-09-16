bdab22f2954614c53acf2d43ee4aacd4414875b8
lfuentess <lfuentessa@gmail.com>
2026-09-15 21:09:21 -0400

    Console App Strings and Integers Assignment
    Create a list of integers. Ask the user for a number to divide each number in the list by. Write a loop that takes each integer in the list, divides it by the number the user entered, and displays the result to the screen.
    
    Make the program print a message to the display to let you know the program has emerged from the try/catch block and continued on with program execution. In the catch block, display the error message to the screen. Then try various combinations of user input: valid numbers, zero and a string. Ensure the proper error messages display on the screen, and that the code after the try/catch block gets executed.

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Strings and Integers 
+
+            //instantion of integer list
+            List<Int64> numbers = new List<Int64>()
+            { 4,9,10,6,20,24,48,64,168 };
+            bool ctrlloop = false;
+
+            //loop control 
+            while (!ctrlloop) 
+            {
+                // begin try/catch
+                try
+                {
+                    Console.WriteLine("Entry a number, please");
+                    Int64 divi = Convert.ToInt64(Console.ReadLine());
+                    // show all number divided for user number
+                    foreach (Int64 num in numbers)
+                    {
+                        Console.WriteLine(num + " divide by " + divi + " equal " + (num / divi) + " \n");
+                    }
+                    ctrlloop = true;
+                }
+                // all the exceptions that could occur and message to the user.
+                catch (FormatException)// if the user type string
+                {
+                    Console.WriteLine("Please, only type numbers.");
+                }
+                catch(DivideByZeroException) // if the user type number 0
+                {
+                    Console.WriteLine("Please, type distinct number of 0");
+                }
+                catch (Exception) // any another error possible 
+                {
+                    Console.WriteLine("Please, check the value inserted.");
+                }
+                finally
+                {
+                    if (!ctrlloop)
+                        Console.WriteLine("Continue... \n");
+                }
+            }
+            //final message 
+            Console.WriteLine("exit to try/catch block.");
+            Console.WriteLine("The 'app' is finished.");
+            Console.ReadLine();
+        }
+    }
+}
