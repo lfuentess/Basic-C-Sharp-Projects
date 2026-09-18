3d71ebd0f6f39aeef600a8c5f43347cd4c03f079
lfuentess <lfuentessa@gmail.com>
2026-09-17 20:00:57 -0400

    Program.cs

---
+++ ConsoleMainMethod/ConsoleMainMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMainMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Main Method
+            //Instantiate commethods class
+            commethods cmenthod = new commethods();
+
+            Console.WriteLine("4 * 3 = {0} ", cmenthod.multiplication(4));// call int multiplication method 
+            Console.WriteLine("4.25 * 3 = {0} ", cmenthod.multiplication(4.25));// call decimal multiplication method
+            Console.WriteLine("'45' * 2 = {0} ", cmenthod.multiplication("45"));// call string multiplication method 
+
+        }
+    }
+}
---
+++ ConsoleMainMethod/ConsoleMainMethod/commethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMainMethod
+{
+    public class commethods
+    {
+        public int multiplication(int dataIn)
+        {
+            return dataIn * 3;
+        }
+        public double multiplication(double dataIn)
+        {
+            return dataIn * 3;
+        }
+        public int multiplication(string dataIn)
+        {
+            return Convert.ToInt32(dataIn)*2;
+        }
+    }
+}
