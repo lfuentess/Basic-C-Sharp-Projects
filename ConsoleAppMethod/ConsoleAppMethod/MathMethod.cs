5c20b9f880c7f53d2591b779dafd362ccb85b406
lfuentess <lfuentessa@gmail.com>
2026-09-18 19:15:24 -0400

    load files into repository Basic_C#_Project

---
+++ ConsoleAppMethod/ConsoleAppMethod/MathMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMethod
+{
+    internal class MathMethod
+    {
+        public int MathOperation(int dataA, int dataB = 2)
+        {
+            return (dataA * dataB);//math operation multiplication.
+        }
+    }
+}
---
+++ ConsoleAppMethod/ConsoleAppMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Method
+            //instantation class
+            MathMethod mathMethod = new MathMethod();
+
+            //require from user first number
+            Console.WriteLine("Enter a number, please");
+            int valueA = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                //require from user second number, but isn't requerid
+                Console.WriteLine("Enter other number, (this number isn't requerid)");
+                int valueB = Convert.ToInt32(Console.ReadLine());
+                //call method with 2 parameters
+                Console.WriteLine(valueA + " * " + valueB +" = "+  mathMethod.MathOperation(valueA, valueB));
+            }
+            catch {
+                //call method with only the first parameter
+                Console.WriteLine(valueA + " * 2 (value x default) = " + mathMethod.MathOperation(valueA));
+            }
+        }
+    }
+}
