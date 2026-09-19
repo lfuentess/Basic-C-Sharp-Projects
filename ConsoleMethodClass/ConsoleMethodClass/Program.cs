258bb42def5232695574f11e1a101136f831f0cf
lfuentess <lfuentessa@gmail.com>
2026-09-19 12:47:30 -0400

    Create a class. Create a void method that takes two integers as parameters. Have the method do a math operation on the first integer and display the second integer to the screen.  (ConsoleMethodClass)
    - Call the method in the class, passing in two numbers.
    - Call the method in the class, specifying the parameters by name.

---
+++ ConsoleMethodClass/ConsoleMethodClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class
+            methodsClass methodsClass = new methodsClass();
+
+            //explain operation #1
+            Console.WriteLine("Call the method in the class, passing in two numbers. \n");
+            methodsClass.method(14, 25);
+
+            //explain operation #2
+            Console.WriteLine("Call the method in the class, specifying the parameters by name. \n");
+            methodsClass.method(dataInA: 40, dataInB: 38);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleMethodClass/ConsoleMethodClass/methodsClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMethodClass
+{
+    class methodsClass
+    {
+        public void method(int dataInA, int dataInB)
+        {
+            //perfom a math operation with de first number (dataInA)
+            Console.WriteLine("\t First number operation: ({0} * 2)  / 4 = {1} \n", dataInA ,(dataInA * 2) / 4);
+            
+            //Show the second number for screen
+            Console.WriteLine("\t Second number is: {0} \n", dataInB);
+        }
+    }
+}
