47fa5c7157396c49fe9c5021abf98e8b1077d4bf
lfuentess <lfuentessa@gmail.com>
2026-09-22 20:23:44 -0400

    Class Method

---
+++ ConsoleClassMethod2/ConsoleClassMethod2/Operations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClassMethod2
+{
+    public class Operations
+    {
+
+        public void Divideby(int dataIn)
+        {
+            //perform the divide operation
+            int resultDivInt = dataIn / 2;
+            //show message on the screen
+            Console.WriteLine("The number {0} divide by 2 is equals {1}", dataIn, resultDivInt);
+        }
+        public void Divideby(decimal dataIn, out decimal dataOut)
+        {
+            //perform the divide operation
+            dataOut = dataIn / 2;
+        }
+
+
+    }
+}
---
+++ ConsoleClassMethod2/ConsoleClassMethod2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClassMethod2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //instantiate the class
+            Operations operations = new Operations();
+            //show message to user
+            showMessage("Enter any number, please \n");
+            showMessage("Your number is integer (i) or decimal (d): \n");
+            //read selection from the user
+            string type = Console.ReadLine();
+            //test the value type
+            if (type == "i")
+            {
+                //declare variable
+                int numberDivInt = 0;
+                //show message to user
+                showMessage("Enter any integer number, please \n");
+
+                //requerid from user a number and convert in integer
+                numberDivInt = Convert.ToInt32(Console.ReadLine());
+                //call the method pass the user number int
+                operations.Divideby(numberDivInt);
+
+            }
+            else if (type =="d")
+            {
+                //declare variables
+                decimal numberDiv = 0;
+                decimal resultDivDec = 0;
+                //show message to user
+                showMessage("Enter any decimal number, please \n");
+                //requerid from user a number and convert in decimal
+                numberDiv = Convert.ToDecimal(Console.ReadLine());
+                //call the method pass the user number decimal
+                operations.Divideby(numberDiv, out resultDivDec);
+                //show message to user
+                Console.WriteLine("The number decimal {0} divide by 2 is equals {1}", numberDiv, resultDivDec);
+
+            }
+            else
+            {
+                //show message to user
+                showMessage("The application only works with whole numbers or decimals.");
+            }
+
+            Console.ReadLine();
+        }
+        public static void showMessage(string messageIn)
+        {
+            Console.WriteLine(messageIn);
+        }
+    }
+}
