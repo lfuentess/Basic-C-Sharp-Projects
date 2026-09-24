cf94d27d8b4f51676adda762cffcf69bb119208a
lfuentess <lfuentessa@gmail.com>
2026-09-24 18:54:01 -0400

    Perform these actions and create a console app that includes the following:
    Create an interface called IQuittable and have it define a void method called Quit().
    
    Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
    
    Use polymorphism to create an object of type IQuittable and call the Quit() method on it. Hint: an object can be of an interface type if it implements that specific interface.

---
+++ ConsolePolymorphism/ConsolePolymorphism/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePolymorphism
+{
+    //Employee class that inherit from Person and IQuittable interface
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+        //constructor to initialize properties
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        //Implementation of the abstract method SayName() of the Person class
+        public override void SayName()
+        {
+            Console.WriteLine("Name: {0} {1} \n", FirstName, LastName);
+        }
+        //Implementation of the Quit() method of the IQuittable interface
+        public void Quit()
+        {
+            Console.WriteLine("Employee Name: {0} {1} \n", FirstName, LastName);
+        }
+    }
+}
---
+++ ConsolePolymorphism/ConsolePolymorphism/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePolymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate an Employee object and past initial data
+            Employee employee1 = new Employee("Sample", "Student");
+
+            //call the SayName method (inherit)
+            employee1.SayName();
+
+            //instantiated as an instance of the Employee class
+            IQuittable quittableEmployee = new Employee("John", "Doe");
+
+            //call the Quit method on the IQuittable object reference
+            quittableEmployee.Quit();
+
+            Console.ReadLine();
+
+        }
+    }
+}
