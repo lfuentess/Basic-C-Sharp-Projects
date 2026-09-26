c119e44fc4d7fb2055b5f7949b9007f1c3ca10d3
lfuentess <lfuentessa@gmail.com>
2026-09-25 21:27:38 -0400

    Create an Employee class with Id, FirstName and LastName properties. In the Employee class, overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property. Remember that comparison operators must be overloaded in pairs.
    In the "Program.cs" file, instantiate two objects of the Employee class and assign values to their properties. Then compare the two Employee objects using the newly overloaded operators and display the results.

---
+++ Operatorsoverload/Operatorsoverload/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorsoverload
+{
+    //Create Employee class
+    public class Employee
+    {
+        //add properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Employee constructor
+        public Employee(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        //== operator overload 
+        public static bool operator == (Employee emp1, Employee emp2)
+        {
+            return emp1.Id == emp2.Id;
+        }
+        //!= operator overload
+        public static bool operator != (Employee emp1, Employee emp2)
+        {
+             return !(emp1.Id != emp2.Id);
+        }
+        // messsage method show the result
+        public static void message(bool dataIn)
+        {
+            if (dataIn)
+            {
+                Console.WriteLine("Result: The Id employees are Equal.");
+            }
+            
+            if (!dataIn)
+            {
+                Console.WriteLine("Result: The Id employees are Not equal.");
+            }
+        }
+    }
+}
---
+++ Operatorsoverload/Operatorsoverload/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorsoverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiation and initialization
+            Employee employeeA = new Employee(15, "Joe", "Doe");
+            Employee employeeB = new Employee(15, "Joe", "Smith");
+            Employee employeeC = new Employee(16, "Jane", "McKey");
+
+            //show message first comparison 
+            Console.WriteLine("Id1 = {0} equal Id2 = {1} ?", employeeA.Id, employeeB.Id);
+
+            //call == operator overload
+            bool comp = employeeA == employeeB;
+            //call show method the result of comparison
+            Employee.message(comp);
+
+            //show message second comparison equal
+            Console.WriteLine("Id1 = {0} equal Id3 = {1} ?", employeeA.Id, employeeC.Id);
+            //
+            bool comp1 = employeeA != employeeC;
+            //call show method the result of comparison
+            Employee.message(comp1);
+        }
+
+    }
+}
